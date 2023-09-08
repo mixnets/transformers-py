a961c9ad9bdcb73b34170d9c17cdb8df4469626f
sd <leowest1921@gmail.com>
2023-09-08 16:39:35 +0800

    add

--- demo.py
+++ demo.py
@@ -6,16 +6,27 @@ from datasets import load_dataset
 import torch
 import soundfile as sf
 
-processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts", )
-model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts", )
-vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan", )
+def readInput():
+    f=open("input.txt", "r", encoding="utf8")
+    list = f.readlines()  # 直接将文件中按行读到list里，效果与方法2一样
+    f.close()  # 关
+    ret = ",".join(list)
+    print(f'xxx {ret}')
+    return ret
 
-inputs = processor(text="Hello, my dog is cute", return_tensors="pt")
+def run():
+        
+    processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts", )
+    model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts", )
+    vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan", )
+    input = readInput()
+    inputs = processor(text=input, return_tensors="pt")
 
-# load xvector containing speaker's voice characteristics from a dataset
-embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
-speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+    # load xvector containing speaker's voice characteristics from a dataset
+    embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+    speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
 
-speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+    speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
 
-sf.write("speech.wav", speech.numpy(), samplerate=16000)
+    sf.write("speech.wav", speech.numpy(), samplerate=16000)
+
