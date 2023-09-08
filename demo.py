4933f9c53df1cee52e14fc3d8b9f651893d2a60a
sd <leowest1921@gmail.com>
2023-09-08 15:29:37 +0800

    add

--- demo.py
+++ demo.py
@@ -5,12 +5,10 @@ from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5Hif
 from datasets import load_dataset
 import torch
 import soundfile as sf
-from datasets import load_dataset
-proxies={'http': '127.0.0.1:1080', 'https': '127.0.0.1:1080'}
 
-processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts", proxies=proxies)
-model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts", proxies=proxies)
-vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan", proxies=proxies)
+processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts", )
+model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts", )
+vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan", )
 
 inputs = processor(text="Hello, my dog is cute", return_tensors="pt")
 
